1c954b67ebc0ea1c4a30cdae29e359af180b0df7
Noah Levenson <noahlevenson@gmail.com>
2017-03-03 16:09:46 -0500

    Modularized

---
+++ luncurve.js
@@ -0,0 +1,137 @@
+/*
+*	lissajous-uncurve
+* 	a node.js tool for animating lissajous curves in the uncurvy ascii terminal
+*
+* 	by noah levenson 
+*
+* 	usage: node luncurve.js a b p [dx dy]
+*
+* 	a = angular frequency a
+* 	b = angular frequency b
+* 	p = initial phase
+* 	dx = x damping constant (optional)
+* 	dy = y damping constant (optional)
+*/
+
+var LISSAJOUS_UNCURVE = (function() {
+    
+	var world = {
+
+    	width: process.stdout.columns,
+
+        height: process.stdout.rows,
+
+        centerX: (function() {
+
+            return Math.ceil(process.stdout.columns / 2);
+
+        })(),
+
+        centerY: (function() {
+
+            return Math.ceil(process.stdout.rows / 2);
+        
+        })()
+
+    };
+
+
+    var screen = {
+
+    	cls: function() {
+
+        process.stdout.write("\033c");
+
+    	},
+
+    	cursorTo: function(x, y) {
+
+        process.stdout.write("\033[" + y + ";" + x + "H");
+    
+    	},
+
+    	hideCursor: function() {
+
+    		this.print("\033[?25l");
+
+    	},
+
+    	out: function(outString) {
+
+        process.stdout.write(outString);
+
+    	},
+
+    	print: function(printString) {
+
+        process.stdout.write(printString + "\n");
+
+    	}
+
+    };
+
+
+    var animation = {
+
+    	delay: 10,
+
+    	precision: 0.1
+
+    };
+
+
+    function LissajousCurve(a, b, p, dx, dy) {
+
+        this.A = Math.ceil(world.height - (world.height * 0.2)) / 2;
+
+        this.B = this.A;
+
+        this.a = a;
+
+        this.b = b;
+
+        this.p = p;
+
+        this.dx = dx;
+
+        this.dy = dy;
+
+        this.x = undefined;
+
+        this.y = undefined;
+
+        this.plot = function(t) {
+
+			this.x = Math.ceil(this.A * Math.cos(this.a * t + this.p) ^ -this.dx * t);
+
+            this.y = Math.ceil(this.B * Math.sin(this.b * t) ^ -this.dy * t);
+
+            return { x: this.x, y: this.y };
+        
+        }
+
+    }
+
+    
+    
+    var figure = new LissajousCurve(2, 3, 0, 0);
+
+    screen.hideCursor();
+
+   	setInterval(function() {
+
+    	screen.cls();
+
+    	for (var t = 0; t < 2000; t += 1) {
+
+    		screen.cursorTo(world.centerX + figure.plot(t).x, world.centerY + figure.plot(t).y);
+
+    		screen.out("●");
+
+    	}
+
+    	figure.p += animation.precision;
+
+    }, animation.delay);
+
+})();
